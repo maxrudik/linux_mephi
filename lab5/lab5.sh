306aed5e2259eed9a255f11a572c75851aceed5d
max <max@localhost.localdomain>
2019-12-24 15:51:53 +0300

    done

--- lab5/lab5.sh
+++ lab5/lab5.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-if [ "$1" != "$2" ]; then
-echo "$3"
-fi
+
+file="lab5.out"
+ps -eo euid, ruid, comm | tail -n +2 >"$file"
+exec 0<"$file"
+
+while read euid ruid name
+do
+	if [[ $euid != $ruid ]]; then
+		echo "ruid and euid of a $name are different"
+	fi
+
+done rm "$file" 
